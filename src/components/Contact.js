50fcd33c930d8b67f4b5d0412bb09416756023be
claudiasanes <claudiasanes@gmail.com>
2020-08-15 15:50:05 +0200

    fix cv

---
+++ src/components/Contact.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../App.css';
+
+function Contact() {
+  return <div className="contact"></div>;
+}
+
+export default Contact;
---
+++ src/components/Projects.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../App.css';
+
+function Projects() {
+  return <div className="projects"></div>;
+}
+
+export default Projects;
